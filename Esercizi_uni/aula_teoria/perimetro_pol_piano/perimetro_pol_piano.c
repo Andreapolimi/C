ec28066cf0f36c91f6088295f10d6c47d4081743
Andreapolimi <vaccai.andrea@gmail.com>
2023-10-17 18:45:10 +0200

    Perimetro poligono terminato

--- Esercizi_uni/aula_teoria/perimetro_pol_piano/perimetro_pol_piano.c
+++ Esercizi_uni/aula_teoria/perimetro_pol_piano/perimetro_pol_piano.c
@@ -2,7 +2,6 @@
 
 #include <stdio.h>
 #include <math.h>
-#include <stdlib.h> //libreria necessaria in questo esercizio esclusivamente per l'utilizzo dell'allocazione dinamica della memoria
 
 typedef struct pt
 {
@@ -11,31 +10,34 @@ typedef struct pt
 
 typedef int contatori;
 
+typedef enum bool
+{
+    False,
+    True
+} bool;
+
 int main()
 {
+    bool controllo;
     contatori i;
-    punto *punti_input;
-    /* per non usare il puntatore si può dichiarare 'punti_input[DIM_MAX]' come array a dimensione prefissata (del tipo
-    '#define DIM_MAX 50') molto alta oltre la quale l'utente non potrà lavorare, per praticità ho usato un vettore dinamico */
-    int dim;
-    float perimetro = 0;
+    int num_punti, pt_fav, indice_partenza, lati_calc, indice_prova;
+    float perimetro = 0, dist_prova, dist_confronto, prima_x, prima_y;
 
     // creazione del vettore di punti
     printf("Inserire il numero di vertici del poligono di cui calcolare il perimetro: ");
-    scanf("%d", &dim);
+    scanf("%d", &num_punti);
 
-    while (dim <= 2)
+    while (num_punti <= 2)
     {
         printf("Inserire una dimensione maggiore a 2 (dimensione di una retta)\n");
         printf("Inserire il numero di vertici del poligono di cui calcolare il perimetro: ");
-        scanf("%d", &dim);
+        scanf("%d", &num_punti);
     }
 
-    // allocazione memoria dinamica, nel caso di vettori statici questo passaggio è da saltare
-    punti_input = (punto *)calloc(dim, dim * sizeof(punto));
+    punto punti_input[num_punti];
 
     // acquisizione punti
-    for (i = 0; i < dim; i++)
+    for (i = 0; i < num_punti; i++)
     {
         printf("Inserisci l'ascissa del %d° punto: ", i + 1);
         scanf("%f", &punti_input[i].x);
@@ -44,17 +46,137 @@ int main()
     }
 
     // calcolo perimetro (o lunghezza nel caso della retta)
-    for (i = 0; i + 1 <= dim; i++)
+    indice_partenza = 0;
+    pt_fav = num_punti;
+    lati_calc = 0;
+    prima_x = punti_input[indice_partenza].x;
+    prima_y = punti_input[indice_partenza].y;
+
+    while (lati_calc != num_punti)
     {
-        if (i == dim)
-            perimetro += sqrt(pow(punti_input[i - dim].x - punti_input[i - 1].x, 2) + pow(punti_input[i - dim].y - punti_input[i - 1].y, 2));
-        else
-            perimetro += sqrt(pow(punti_input[i].x - punti_input[i + 1].x, 2) + pow(punti_input[i].y - punti_input[i + 1].y, 2));
+        // inizializzo variabili utili per trovare i punti da usare per il calcolo di ogni lato
+        controllo = False;
+
+        // controllo punti in senso anti-orario con x maggiore, i punti favorevoli saranno quelli più vicini a quello iniziale
+
+        // controllo dei punti di indice maggiore di quello di partenza
+        for (i = indice_partenza + 1; i < pt_fav; i++)
+        {
+            if (punti_input[i].x >= punti_input[indice_partenza].x)
+            {
+                if (controllo == False)
+                {
+                    indice_prova = i;
+                    controllo = True;
+                    dist_prova = sqrt(pow(punti_input[indice_partenza].x - punti_input[i].x, 2) + pow(punti_input[indice_partenza].y - punti_input[i].y, 2));
+                }
+                else
+                {
+                    dist_confronto = sqrt(pow(punti_input[indice_partenza].x - punti_input[i].x, 2) + pow(punti_input[indice_partenza].y - punti_input[i].y, 2));
+                    if (dist_prova > dist_confronto)
+                    {
+                        indice_prova = i;
+                        dist_prova = dist_confronto;
+                    }
+                }
+            }
+        }
+
+        // controllo dei punti di indice minore di quello di partenza
+        for (i = indice_partenza - 1; i >= 0; i--)
+        {
+            if (punti_input[i].x >= punti_input[indice_partenza].x)
+            {
+                if (controllo == False)
+                {
+                    indice_prova = i;
+                    controllo = True;
+                    dist_prova = sqrt(pow(punti_input[indice_partenza].x - punti_input[i].x, 2) + pow(punti_input[indice_partenza].y - punti_input[i].y, 2));
+                }
+                else
+                {
+                    dist_confronto = sqrt(pow(punti_input[indice_partenza].x - punti_input[i].x, 2) + pow(punti_input[indice_partenza].y - punti_input[i].y, 2));
+                    if (dist_prova > dist_confronto)
+                    {
+                        indice_prova = i;
+                        dist_prova = dist_confronto;
+                    }
+                }
+            }
+        }
+
+        // controllo in senso antiorario con x minore se non mi erano punti in senso orario, i punti favorevoli saranno quelli più vicini a quello iniziale
+
+        if (controllo == False)
+        {
+            // controllo dei punti di indice maggiore di quello di partenza
+            for (i = indice_partenza + 1; i < pt_fav; i++)
+            {
+                if (punti_input[i].x <= punti_input[indice_partenza].x)
+                {
+                    if (controllo == False)
+                    {
+                        indice_prova = i;
+                        dist_prova = sqrt(pow(punti_input[indice_partenza].x - punti_input[i].x, 2) + pow(punti_input[indice_partenza].y - punti_input[i].y, 2));
+                    }
+                    else
+                    {
+                        dist_confronto = sqrt(pow(punti_input[indice_partenza].x - punti_input[i].x, 2) + pow(punti_input[indice_partenza].y - punti_input[i].y, 2));
+                        if (dist_prova > dist_confronto)
+                        {
+                            indice_prova = i;
+                            dist_prova = dist_confronto;
+                        }
+                    }
+                }
+            }
+
+            // controllo dei punti di indice minore di quello di partenza
+
+            for (i = indice_partenza - 1; i >= 0; i--)
+            {
+                if (punti_input[i].x <= punti_input[indice_partenza].x)
+                {
+                    if (controllo == False)
+                    {
+                        indice_prova = i;
+                        dist_prova = sqrt(pow(punti_input[indice_partenza].x - punti_input[i].x, 2) + pow(punti_input[indice_partenza].y - punti_input[i].y, 2));
+                    }
+                    else
+                    {
+                        dist_confronto = sqrt(pow(punti_input[indice_partenza].x - punti_input[i].x, 2) + pow(punti_input[indice_partenza].y - punti_input[i].y, 2));
+                        if (dist_prova > dist_confronto)
+                        {
+                            indice_prova = i;
+                            dist_prova = dist_confronto;
+                        }
+                    }
+                }
+            }
+        }
+
+        // calcolo perimetro
+        perimetro += dist_prova;
+        lati_calc++;
+
+        // elimino il punto di partenza già utiliizzato
+        for (i = indice_partenza; i < pt_fav - 1; i++)
+            punti_input[i] = punti_input[i + 1];
+
+        pt_fav--;
+
+        // inizializzo il prossimo punto di partenza
+        indice_partenza = indice_prova;
+
+        // controllo se manca calcolare solo l'ultimo lato
+        if (pt_fav == 1)
+        {
+            perimetro += sqrt(pow(punti_input[indice_partenza].x - prima_x, 2) + pow(punti_input[indice_partenza].y - prima_y, 2));
+            lati_calc++;
+        }
     }
 
     printf("Il perimetro ha dimensione %f", perimetro);
-
-    free(punti_input); // liberazione memoria dinamica, se si usa il vettore statico è da saltare
 
     return 0;
 }
